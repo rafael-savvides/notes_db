036df66717abfe3e1351ce836892f3b02f2ca3a9
Rafael Savvides <savvidesrafael@gmail.com>
2022-02-05 22:05:31 +0200

    Refactor init_db, use files

--- init_db.py
+++ init_db.py
@@ -1,13 +1,26 @@
 import sqlite3
+import os
+from secret import notes_path
 
-db_connection = sqlite3.connect('notes.db')
+def insert_to_db(cursor, text):
+    cursor.execute("INSERT INTO notes (content) VALUES (?)", (text, ))
 
-with open('schema.sql') as f:
-    db_connection.executescript(f.read())
+def insert_notes_to_db(cursor, notes_path):
+    note_path_list = [p for p in os.listdir(notes_path) if p.endswith('.md')]
+    for note_name in note_path_list:
+        note_name_abs = os.path.join(notes_path, note_name)
+        with open(note_name_abs, encoding='utf8') as f:
+            content = f.read()
+        insert_to_db(cursor, content)
 
-cursor = db_connection.cursor()
-cursor.execute("INSERT INTO notes (content) VALUES (?)", ('# The First Note',))
-cursor.execute("INSERT INTO notes (content) VALUES (?)", ('_Another note_',))
+if __name__ == "__main__": 
+    db_connection = sqlite3.connect('notes.db')
 
-db_connection.commit()
-db_connection.close()
+    with open('schema.sql') as f:
+        db_connection.executescript(f.read())
+
+    cursor = db_connection.cursor()
+    insert_notes_to_db(cursor, notes_path)
+
+    db_connection.commit() # If you don’t call this method, anything you did since the last call to commit() is not visible from other database connections. https://docs.python.org/3/library/sqlite3.html#sqlite3.Connection
+    db_connection.close()
